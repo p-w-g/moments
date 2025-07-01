ab8a15a680cef31b1c178c04d7711dc078ce7ca3
p-w-g <p.grzelczyk@gmail.com>
2025-07-01 20:28:00 +0200

    add netlify adapter config

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
+	kit: { adapter: adapter({ edge: false }) },
 	extensions: ['.svelte', '.svx']
 };
 
