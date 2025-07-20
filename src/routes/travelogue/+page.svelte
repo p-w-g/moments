831150592826df5f65c2832d2ce21612fcf5f2db
p-w-g <p.grzelczyk@gmail.com>
2025-07-20 14:29:39 +0200

    group up moments

--- src/routes/travelogue/+page.svelte
+++ src/routes/travelogue/+page.svelte
@@ -1,10 +1,21 @@
 <script lang="ts">
-	import WordCloud from '$lib/WordCloud.svelte';
 	import MomentFilters from '$lib/MomentFilters.svelte';
-
-	export let data: { posts: any[] };
+	export let data;
+	const { chapters, moments } = data;
 </script>
 
 <MomentFilters tags={data.tags} chapters={data.chapters} active={data.active} />
 
-<WordCloud words={data.posts} />
+{#each chapters as chapter}
+	{#if data.active.chapter === null || chapter.title.toLowerCase() === data.active.chapter.toLowerCase()}
+		<h3>{chapter.title}</h3>
+		<ul>
+			{#each moments.filter((m) => m.chapter.slug === chapter.slug) as m}
+				<li><a href={`/travelogue/${m.slug}`}>{m.title}</a></li>
+			{/each}
+		</ul>
+	{/if}
+{/each}
+
+<style>
+</style>
