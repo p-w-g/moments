5e9153547076ff9b494f802fcf9d00e0209e462d
p-w-g <p.grzelczyk@gmail.com>
2025-07-20 21:49:07 +0200

    prune every redundancies

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,7 @@
 <script>
 	import Navbar from './navbar.svelte';
 	import '../app.css';
-	// import { page } from '$app/state';
-
-	// $: heroMode = page.url.pathname === '/';
-	// $: heroMode = $page.data?.hero || $page.url.pathname === '/';
-	// $: wrapperClasses = heroMode ? 'frame' : ''; // add more if needed
 </script>
 
-<!-- <div class={wrapperClasses}> -->
-<div class={''}>
-	<Navbar />
-	<main class="flex-1 p-6">
-		<slot />
-	</main>
-</div>
+<Navbar />
+<slot />
