dcf28bee8cf45fc5bca5f5e0931877022a478ce5
p-w-g <p.grzelczyk@gmail.com>
2025-08-02 15:36:32 +0200

    removal of useless aside

--- src/routes/highlights/[slug]/+page.svelte
+++ src/routes/highlights/[slug]/+page.svelte
@@ -18,20 +18,7 @@
 			<img
 				src={urlFor(data.highlight.image).width(768).auto('format').url()}
 				alt={data.highlight.title}
-				class="w-full h-auto rounded-lg shadow"
 			/>
 		</picture>
-		<!-- screen-reader caption only -->
-		<figcaption class="sr-only">{data.highlight.title}</figcaption>
 	</figure>
-	<aside class="meta">
-		<h1>{h.title}</h1>
-		<p>{h.notes}</p>
-
-		<!-- <ul>
-			<li>Taken: {h.timestamp.slice(0, 10)}</li>
-			<li>Camera: {h.exif?.model}</li> -->
-		<!-- …whatever else you store… -->
-		<!-- </ul> -->
-	</aside>
 </div>
